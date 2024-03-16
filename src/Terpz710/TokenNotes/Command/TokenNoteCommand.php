86109d907086653a488895955e048ecc22317782
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-03-16 16:26:01 -0700

    Fixes

--- src/Terpz710/TokenNotes/Command/TokenNoteCommand.php
+++ src/Terpz710/TokenNotes/Command/TokenNoteCommand.php
@@ -7,55 +7,60 @@ namespace Terpz710\TokenNotes\Command;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginOwned;
 use pocketmine\item\Item;
-use pocketmine\item\VanillaItems;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\IntTag;
 
-class TokenNoteCommand extends Command {
+use Terpz710\TokenNotes\Loader;
+use Terpz710\TokensAPI\API\TokenAPI;
+use Terpz710\TokensAPI\Tokens;
 
-    public function __construct() {
-        parent::__construct("tokennote", "Create a bank note with a specified value", "/banknote <amount>");
+class TokenNoteCommand extends Command implements PluginOwned {
+
+    private $plugin;
+
+    public function __construct(Loader $plugin) {
+        parent::__construct("tokennotes", "Give a player a token note!", "/tokennotes [amount]", ["tn"]);
+        $this->plugin = $plugin;
         $this->setPermission("tokennotes.cmd");
+    }
+
+    public function getOwningPlugin(): Plugin {
+        return $this->plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         if (!$sender instanceof Player) {
             $sender->sendMessage("This command can only be used in-game!");
-            return false;
-        }
-        
-        if (count($args) !== 1 || !is_numeric($args[0])) {
-            $sender->sendMessage("Usage: /banknote <amount>");
-            return false;
-        }
-
-        $amount = (int) $args[0];
-        if ($amount <= 0) {
-            $sender->sendMessage("Please specify a positive amount!");
-            return false;
-        }
-
-        $bankNote = $this->createBankNoteItem($amount);
-        if ($bankNote !== null) {
-            $sender->getInventory()->addItem($bankNote);
-            $sender->sendMessage("Token note with value §e{$amount} tokens§f created!");
             return true;
-        } else {
-            $sender->sendMessage("Failed to create token note! Please contact §bTerpz710§f on discord: §eace873056. §for§e ace87.");
-            return false;
         }
-    }
 
-    private function createBankNoteItem(int $value) {
-        $bankNote = VanillaItems::PAPER();
-        $bankNote->setCustomName("Bank Note $" . $value);
-        $nbt = new CompoundTag("", [
-            "BankNote" => new CompoundTag("BankNote", [
-                "Value" => new IntTag("Value", (int)$value)
-            ])
-        ]);
-        $bankNote->setNamedTag($nbt);
-        return $bankNote;
+        if (count($args) !== 1) {
+            $sender->sendMessage("Usage:§e /tokennote [amount]");
+            return true;
+        }
+
+        $targetPlayer = $sender; // The sender is now the target player
+        $tokenAmount = (int)$args[0];
+
+        if ($tokenAmount <= 0) {
+            $sender->sendMessage("§l§c(!)§r§f Amount must be a positive number!");
+            return true;
+        }
+
+        $senderTokens = TokenAPI::getPlayerToken($sender);
+        if ($senderTokens < $tokenAmount) {
+            $sender->sendMessage("§l§c(!)§r§f You don't have enough tokens to create this Token Note!");
+            return true;
+        }
+        TokenAPI::removeToken($sender, $tokenAmount);
+
+        $tokenNote = $this->plugin->createTokenNote($tokenAmount);
+        $targetPlayer->getInventory()->addItem($tokenNote);
+
+        $sender->sendMessage("§l§a(!)§r§f Token note successfully created and given to §e" . $targetPlayer->getName() . "§f!");
+
+        return true;
     }
 }
